3e67adc370a2a22b146af732e0c55689595e0357
Dev-Reve <wodnrla635@naver.com>
2023-09-04 17:56:48 +0900

    DTO 작업

---
+++ WebContent/Professor/Info.jsp
@@ -0,0 +1,47 @@
+<%@page import="Professor.ProBean"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<%
+	request.setCharacterEncoding("UTF-8");
+
+	ProBean pb = new ProBean();
+	String no = request.getParameter("no");
+	
+%>
+
+<form action = "ProModify.jsp" method = "post">
+	<div align="center">
+	<h1>교수 정보</h1>
+	<table>
+		<tr>
+			<th>교수 아이디</th>
+			<td>id</td>
+		</tr>
+		<tr>
+			<th>교수 이름</th>
+			<td></td>
+		</tr>
+	
+
+<!-- 	private int no; // 교수 식별 번호 -->
+<!-- 	private String id;	//교수 아이디 -->
+<!-- 	private String pw;	// 교수 비밀번호 -->
+<!-- 	private String name; // 교수 이름  -->
+<!-- 	private String addr; // 교수 거주지 -->
+<!-- 	private String tel; // 연구실 전화번호 -->
+<!-- 	private String phone; // 교수 휴대전화 번호 -->
+<!-- 	private String major; // 학부(전공) -->
+<!-- 	private String email; //교수 이메일 주소 -->
+<!-- 	private String labAddr; // 연구실 번 -->
+	</table>
+	</div>
+</form>
+</body>
+</html>
---
+++ src/Professor/ProDao.java
@@ -0,0 +1,41 @@
+package Professor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.sql.DataSource;
+
+public class ProDao {
+
+		private Connection con;
+		private PreparedStatement pstmt;
+		private ResultSet rs;
+		private DataSource ds;
+		
+		private Connection getConnection() throws Exception {
+			
+			//1. InitialContext객체 생성
+			//생성하는 이유는  자바의 네이밍 서비스(JNDI)에서 이름과 실제 객체를 연결해주는 개념이 Context이며,
+			//InitialContext객체는 네이밍 서비스를 이용하기위한 시작점입니다.
+			Context initCtx = new InitialContext();
+			//2. "java:comp/env"라는 주소를 전달하여  Context객체를 얻었습니다.
+			//"java:comp/env" 주소는 현재 웹 애플리케이션의 루트 디렉터리 라고 생각 하면됩니다.
+			//즉! 현재 웹애플리케이션이 사용할수 있는 모든 자원은 "java:comp/env"아래에 위치합니다.(<Context></Context/>이위치를 말합니다.)
+			Context ctx = (Context)initCtx.lookup("java:comp/env");
+			//3. "java:comp/env 경로 아래에 위치한  "jdbc/jspbeginner" Recource태그의  DataSource커넥션풀을 얻는다
+			ds = (DataSource)ctx.lookup("jdbc/jspbeginner");		 
+			//4. 마지막으로 커넥션풀(DataSouce)객체 메모리 에 저장된 Connection객체를 반환받아 사용
+			con = ds.getConnection();
+			return con;
+		}
+		
+		public void ProInfo(ProBean pb) {
+			
+			
+			
+			
+		}
+}
