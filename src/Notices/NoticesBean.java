f27f26a7ca86ce99ac38a54d33ea657053fc7997
soeek <eek38@naver.com>
2023-10-06 18:16:35 +0900

    a

--- src/Notices/NoticesBean.java
+++ src/Notices/NoticesBean.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package Notices;
 
 public class NoticesBean {
@@ -51,57 +51,4 @@ public class NoticesBean {
 	
 	
 }
-=======
-package Notices;
 
-public class NoticesBean {
-
-	private int num;
-	private String title;        
-	private String content;      
-	private String regdate;     
-	private int sub_no;            
-	private int pro_no;
-
-	
-	public int getNum() {
-		return num;
-	}
-	public void setNum(int num) {
-		this.num = num;
-	}
-	public String getTitle() {
-		return title;
-	}
-	public void setTitle(String title) {
-		this.title = title;
-	}
-	public String getContent() {
-		return content;
-	}
-	public void setContent(String content) {
-		this.content = content;
-	}
-	public String getRegdate() {
-		return regdate;
-	}
-	public void setRegdate(String regdate) {
-		this.regdate = regdate;
-	}
-	public int getSub_no() {
-		return sub_no;
-	}
-	public void setSub_no(int sub_no) {
-		this.sub_no = sub_no;
-	}
-	public int getPro_no() {
-		return pro_no;
-	}
-	public void setPro_no(int pro_no) {
-		this.pro_no = pro_no;
-	}        
-	
-	
-	
-}
->>>>>>> 281df3d6eaca0dc18cca82b395222ad9e4fc3678
